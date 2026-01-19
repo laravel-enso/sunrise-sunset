0db4f7e2214507e43222249cf004bc09455d97c0
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2026-01-19 16:56:54 +0200

    small change

--- database/migrations/2026_01_15_101000_create_structure_for_sunrise_sunset.php
+++ database/migrations/2026_01_15_101000_create_structure_for_sunrise_sunset.php
@@ -2,8 +2,9 @@
 
 use LaravelEnso\Migrator\Database\Migration;
 
-return new class extends Migration {
+return new class extends Migration
+{
     protected array $permissions = [
-        ['name' => 'sunriseSunset.index', 'description' => 'Call Api Sunrise Sunset', 'is_default' => false],
+        ['name' => 'sunriseSunset.index', 'description' => 'Call Api Sunrise-Sunset', 'is_default' => false],
     ];
 };
